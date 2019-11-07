09bb348fdccf32ade56426babb74b0ce03e7ad8c
Maika Kailani <queenmab051@gmail.com>
2019-11-07 18:18:05 +0000

    Done.

---
+++ greeting.rb
@@ -0,0 +1,11 @@
+# Methods can define new routines and procedures by defining a new routine or method it can execute using the provided variables.
+
+def greeting
+  puts "Hello World"
+end
+
+greeting
+greeting
+greeting
+greeting
+greeting
